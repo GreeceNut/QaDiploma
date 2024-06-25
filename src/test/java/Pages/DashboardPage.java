c9519480c1e5f4865abda35811a76a5e2b732626
Burenkov Oleg <duncanscoffee@gmail.com>
2024-06-25 17:33:35 +0200

    added tests

---
+++ src/test/java/Pages/DashboardPage.java
@@ -0,0 +1,24 @@
+package Pages;
+
+import Pays.CreditCard;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DashboardPage {
+    private final SelenideElement payButton = $$(".button")
+            .find(exactText("Купить"));
+    private SelenideElement creditButton = $$(".button")
+            .find(text("Купить в кредит"));
+
+    public PayPage clickPay() {
+        payButton.click();
+        return new PayPage();
+    }
+
+    public PayPage clickCredit() {
+        creditButton.click();
+        return new PayPage();
+    }
+}
---
+++ src/test/java/data/PaymentStatus.java
@@ -0,0 +1,12 @@
+package data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PaymentStatus {
+
+        private String status;
+    }
